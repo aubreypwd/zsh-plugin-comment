e67a3e913001c25fd9fad097182e0dba186a4740
Aubrey Portwood <code@aubreypwd.com>
2020-10-01 12:25:08 -0600

    Detect macos.

--- comment.plugin.zsh
+++ comment.plugin.zsh
@@ -9,5 +9,10 @@
  # @since Thursday, 10/1/2020
  ##
 function comment {
+	if ! [[ -x $(command -v osascript) ]]; then
+		echo "This works on macOS only, or this requires osascript."
+		return
+	fi
+
 	osascript -e 'on run {f, c}' -e 'tell app "Finder" to set comment of (POSIX file f as alias) to c' -e end "$1" "$2" > /dev/null 2>&1
 }
